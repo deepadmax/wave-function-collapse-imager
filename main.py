907f85f786f0949d2a0945514bd22dee22944695
deepadmax <maximillian.strand@gmail.com>
2020-10-22 04:23:16 +0200

    updated main

--- main.py
+++ main.py
@@ -1,12 +1,14 @@
 from src import Field
 
-field = Field()
+field = Field.create(
+    'demo.pat',
+    width=32,
+    height=9
+)
 
-field.load_from_file('demo.pat')
-field.load_patterns()
 field.clear()
 
-field.seed()
+# field.seed()
 
-field.generate()
-# print(board)
+field.collapse()
+print(field)
